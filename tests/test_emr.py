f605a5baf56c7addfef1f9d7caba5057c32c5640
Kalaiselvan Krishnan <kalaiselvan_k@intuit.com>
2023-01-18 22:21:09 +0530

    aws - filter to annotate security configuration and filter with attributes

--- tests/test_emr.py
+++ tests/test_emr.py
@@ -166,6 +166,29 @@ class TestEMR(BaseTest):
         self.assertEqual(len(resources), 1)
         self.assertEqual(resources[0]["Name"], "pratyush-emr-test")
 
+    def test_emr_security_configuration(self):
+        session_factory = self.replay_flight_data("test_emr_sc")
+        p = self.load_policy(
+            {
+                "name": "emr-sc-filter",
+                "resource": "emr",
+                "filters": [
+                    {
+                        "type": "security-configuration",
+                        "key": "EnableAtRestEncryption",
+                        "value": True
+                    }
+                ],
+            },
+            config={"region": "us-west-2"},
+            session_factory=session_factory
+        )
+        resources = p.run()
+        self.assertEqual(len(resources), 1)
+        self.assertEqual(resources[0]
+                         ["c7n:SecurityConfiguration"]
+                         ["EnableAtRestEncryption"], True)
+
 
 class TestEMRQueryFilter(unittest.TestCase):
 
@@ -251,7 +274,6 @@ class TestEMRSecurityConfiguration(BaseTest):
             },
             session_factory=session_factory)
         resources = p.run()
-        print(resources)
         self.assertEqual(resources[0]["SecurityConfiguration"]['EncryptionConfiguration']
              ['EnableInTransitEncryption'], False)
 
